8231de7d7d83d7999134d91cfaa952ac02511938
Magesh Kuppan <tkmagesh77@gmail.com>
2025-08-19 15:45:32 +0530

    channel behaviors

---
+++ 01-concurrency/05-communication/12-demo.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	/*
+		ch := make(chan int)
+		data := <-ch
+		ch <- 100
+		fmt.Println(data)
+	*/
+
+	/*
+		ch := make(chan int)
+		ch <- 100
+		data := <-ch
+		fmt.Println(data)
+	*/
+
+	/*
+		ch := make(chan int)
+		go func() {
+			ch <- 100
+		}()
+		data := <-ch
+		fmt.Println(data)
+	*/
+
+	ch := make(chan int)
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		data := <-ch
+		time.Sleep(2 * time.Second)
+		fmt.Println(data)
+	}()
+	ch <- 100
+	wg.Wait()
+}
